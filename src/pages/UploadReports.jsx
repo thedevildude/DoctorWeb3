30d95e9db099a974f9d463ba7eacbc5d099ff64b
Devdeep Ghosh <devarmforce01@gmail.com>
2022-11-23 19:47:58 +0530

    update abi

--- src/pages/UploadReports.jsx
+++ src/pages/UploadReports.jsx
@@ -1,5 +1,6 @@
 import "../css/UploadReports.css";
 import { useState } from "react";
+import { create } from "ipfs-http-client";
 
 const UploadReports = () => {
 
@@ -31,65 +32,73 @@ const UploadReports = () => {
   const handleFormSubmit = async (e) => {
     e.preventDefault()
     console.log(selectedFile.name);
+    let ipfs = await ipfsClient()
+    const {cid} = await ipfs.add(selectedFile)
+    console.log(cid);
     setSelectedFile("")
   }
 
-    return (
-      <div className="upload-reports">
-        <h1 className="upload-reports-form-title">Upload Reports</h1>
-        <div className="upload-reports-form-container">
-          <form className="upload-reports-form">
-            <input
-              type="string"
-              value={category}
-              placeholder="Enter category of report"
-              onChange={e => setCategory(e.target.value)}
-            />
-            <input
-              type="number"
-              value={age}
-              placeholder="Enter age"
-              onChange={e => setAge(e.target.value)}
-            />
-            <input
-              type="number"
-              value={weight}
-              placeholder="Enter weight"
-              onChange={e => setWeight(e.target.value)}
-            />
-            <input
-              type="number"
-              value={height}
-              placeholder="Enter height"
-              onChange={e => setHeight(e.target.value)}
-            />
-            <input
-              type="string"
-              value={gender}
-              placeholder="Enter gender"
-              onChange={e => setGender(e.target.value)}
-            />
-            <input
-              type="string"
-              value={bloodgroup}
-              placeholder="Enter bloodgroup"
-              onChange={e => setBloodgroup(e.target.value)}
-            />
-            <input
-              type="file"
-              onChange={handleFileInput}
-            />
-            <input
-              type="password"
-              value={password}
-              placeholder="Enter password to lock your report"
-              onChange={e => setPassword(e.target.value)}
-            />
-            {selectedFile !== undefined || "" ? <button type="submit" onClick={handleFormSubmit} className="button-18">Upload Report</button> : <button disabled className="button-18">Upload Report</button>}
-          </form>
-        </div>
-      </div>
-    )
+  const ipfsClient = async () => {
+    const ipfs = create(new URL('http://127.0.0.1:5001/k51qzi5uqu5dm4otgfznoq4jl8xavfscsgg6fqquyk8fncrdvq4pqwwdpi7jhy'))
+    return ipfs
   }
 
-  export default UploadReports;
+  return (
+    <div className="upload-reports">
+      <h1 className="upload-reports-form-title">Upload Reports</h1>
+      <div className="upload-reports-form-container">
+        <form className="upload-reports-form">
+          <input
+            type="string"
+            value={category}
+            placeholder="Enter category of report"
+            onChange={e => setCategory(e.target.value)}
+          />
+          <input
+            type="number"
+            value={age}
+            placeholder="Enter age"
+            onChange={e => setAge(e.target.value)}
+          />
+          <input
+            type="number"
+            value={weight}
+            placeholder="Enter weight"
+            onChange={e => setWeight(e.target.value)}
+          />
+          <input
+            type="number"
+            value={height}
+            placeholder="Enter height"
+            onChange={e => setHeight(e.target.value)}
+          />
+          <input
+            type="string"
+            value={gender}
+            placeholder="Enter gender"
+            onChange={e => setGender(e.target.value)}
+          />
+          <input
+            type="string"
+            value={bloodgroup}
+            placeholder="Enter bloodgroup"
+            onChange={e => setBloodgroup(e.target.value)}
+          />
+          <input
+            type="file"
+            onChange={handleFileInput}
+          />
+          <input
+            type="password"
+            value={password}
+            placeholder="Enter password to lock your report"
+            onChange={e => setPassword(e.target.value)}
+          />
+          {selectedFile !== undefined || "" ? <button type="submit" onClick={handleFormSubmit} className="button-18">Upload Report</button> : <button disabled className="button-18">Upload Report</button>}
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default UploadReports;
