7b75187188d4675e3c856a39001326f20fb2b676
Devdeep Ghosh <devarmforce01@gmail.com>
2023-11-13 02:39:22 +0530

    my reports fix

--- src/pages/MyReports.jsx
+++ src/pages/MyReports.jsx
@@ -1,50 +1,67 @@
-import { useState, useContext } from "react";
-import ContractContext from "../ContractContext";
-import "../css/MyReports.css"
+import { useEffect, useState } from "react";
+import { useContextState } from "../context/context";
+import "../css/MyReports.css";
+import { ColorRing } from "react-loader-spinner";
 
 const MyReports = () => {
+  const { isLoading, isConnected, doctorWeb3 } = useContextState();
+  const [reports, setReports] = useState([]);
+  const [reportsLoading, setReportsLoading] = useState(true);
 
-  const { doctorWeb3 } = useContext(ContractContext)
-  const [reports, setReports] = useState([])
-  const [loading, setLoading] = useState(true)
-
-
-  const FindReports = async () => {
-    const reports = await doctorWeb3.getPatientReports()
-    DetailedReport(reports)
-  }
-
-  const DetailedReport = async (reports) => {
-    let detailedReport = []
-    for (let i = 0; i < reports.length; i++) {
-      const data = await doctorWeb3.getDetailedReports(reports[i])
-      detailedReport.push([data[0], data[1]])
-    }
-    setReports(detailedReport)
-    setLoading(false)
-  }
+  useEffect(() => {
+    const getDetailedReport = async () => {
+      if (isLoading || !isConnected) return;
+      setReportsLoading(true);
+      const reports = await doctorWeb3.getPatientReports();
+      let detailedReport = [];
+      for (let i = 0; i < reports.length; i++) {
+        const data = await doctorWeb3.getDetailedReports(reports[i]);
+        detailedReport.push([data[0], data[1]]);
+      }
+      setReports(detailedReport);
+      setReportsLoading(false);
+    };
+    getDetailedReport();
+  }, [doctorWeb3, isConnected, isLoading]);
 
   return (
     <div className="my-reports">
       <h1 className="my-reports-title">My Reports</h1>
-      <div className="my-reports-button">
-        <button className="button-18" onClick={FindReports}>Load Reports</button>
-      </div>
       <div className="reports-box">
-        {loading ? <p className='loading-text'>Loading...</p> : reports.map((item, index) => {
-          return (
-            <div key={index} className="report-box">
-              <div className="report-filehash-box">
-                <p className="filehash-box-title">FileHash</p>
-                <p className="filehash-box-filehash"><a href={`http://127.0.0.1:9090/ipfs/${item[0]}`} target="_blank" rel="noopener noreferrer">{item[0]}</a></p>
+        {isLoading || !isConnected || reportsLoading ? (
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+        ) : (
+          reports.map((item, index) => {
+            return (
+              <div key={index} className="report-box">
+                <div className="report-filehash-box">
+                  <p className="filehash-box-title">FileHash</p>
+                  <p className="filehash-box-filehash">
+                    <a
+                      href={`http://127.0.0.1:9090/ipfs/${item[0]}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {item[0]}
+                    </a>
+                  </p>
+                </div>
+                <p className="report-box-category">{item[1]}</p>
               </div>
-              <p className="report-box-category">{item[1]}</p>
-            </div>
-          )
-        })}
+            );
+          })
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MyReports;
