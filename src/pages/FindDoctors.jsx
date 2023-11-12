c00da93b871dcdd11dd59c5bb6b5c8c20ed499b6
Devdeep Ghosh <devarmforce01@gmail.com>
2023-11-13 02:15:17 +0530

    registered doctors fix

--- src/pages/FindDoctors.jsx
+++ src/pages/FindDoctors.jsx
@@ -1,48 +1,61 @@
-import { useState, useContext } from 'react';
-import ContractContext from '../ContractContext';
-import '../css/findDoctorHospital.css'
+import { useEffect, useState } from "react";
+import { useContextState } from "../context/context";
+import "../css/findDoctorHospital.css";
+import { ColorRing } from "react-loader-spinner";
 
 const FindDoctors = () => {
+  const { isLoading, isConnected, doctorWeb3 } = useContextState();
+  const [doctorDetails, setDoctorDetails] = useState([]);
 
-  const { doctorWeb3 } = useContext(ContractContext)
-  const [doctorDetails, setDoctorDetails] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  const findDHDetails = async () => {
-    const doctorAddress = await doctorWeb3.findDoctors()
-    const detailArray = [];
-    for (let i = 0; i < await doctorAddress.length; i++) {
-      let helpObject = {}
-      const data = await doctorWeb3.getAuthorizedDHDetails(doctorAddress[i])
-      data.forEach((element, index) => {
-        helpObject['detail' + index] = element;
-      });
-      detailArray.push(helpObject)
-    }
-    setDoctorDetails(detailArray)
-    setLoading(false)
-
-  }
+  useEffect(() => {
+    const findDHDetails = async () => {
+      const doctorAddress = await doctorWeb3.findDoctors();
+      const detailArray = [];
+      for (let i = 0; i < (await doctorAddress.length); i++) {
+        let helpObject = {};
+        const data = await doctorWeb3.getAuthorizedDHDetails(doctorAddress[i]);
+        data.forEach((element, index) => {
+          helpObject["detail" + index] = element;
+        });
+        detailArray.push(helpObject);
+      }
+      setDoctorDetails(detailArray);
+    };
+    findDHDetails();
+  }, [doctorWeb3]);
 
   return (
-    <div className='findDH'>
-      <h1 className='findDH-title'>Registered Doctors</h1>
-      <div className='findDH-button'>
-        <button className='button-18' onClick={() => findDHDetails(0)}>Click to load Doctors</button>
-      </div>
-      <div className='findDH-cards'>
-        {loading ? <p className='loading-text'>Loading...</p> : doctorDetails.map((item, index) => {
-          return (
-            <div key={index} className="findDH-card">
-              <img className='findDH-card-image' src='https://via.placeholder.com/200' alt='' />
-              <p className='findDH-card-name'>{item.detail1}</p>
-              <p className='findDH-card-address'>{item.detail0}</p>
-            </div>
-          )
-        })}
+    <div className="findDH">
+      <h1 className="findDH-title">Registered Doctors</h1>
+      <div className="findDH-cards">
+        {isLoading || !isConnected ? (
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+        ) : (
+          doctorDetails.map((item, index) => {
+            return (
+              <div key={index} className="findDH-card">
+                <img
+                  className="findDH-card-image"
+                  src="https://via.placeholder.com/200"
+                  alt=""
+                />
+                <p className="findDH-card-name">{item.detail1}</p>
+                <p className="findDH-card-address">{item.detail0}</p>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default FindDoctors;
